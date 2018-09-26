a708f3f589edb3e9ca3dbeeaf95e95783aeddc6e
matttwilkes <matthew_wilkes7@hotmail.com>
2018-09-25 20:43:22 -0700

    reactbasics

---
+++ Week01-ReactBasics/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactBasics } from './ReactBasicsStatelessFunctional.js';
+
+
+ReactDOM.render(
+    <ReactBasics/>,
+    document.getElementById("root")
+);
