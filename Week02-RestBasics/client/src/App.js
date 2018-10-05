34aeb1dbdc5cc354b6441c980afe873278c3a464
matttwilkes <matthew_wilkes7@hotmail.com>
2018-10-04 22:44:32 -0700

    RestTests

--- Week02-RestBasics/client/src/App.js
+++ Week02-RestBasics/client/src/App.js
@@ -7,22 +7,22 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            file: 'File name will be placed here.',
-            status: 'waiting for server'
+            file: 'unknown',
+            status: 'waiting'
         };
     }
 
-    queryServer = () => {
+    bar = () => {
         const that = this;
         fetch('/api/foo')
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
-            .then(function(json) {
+            .then(function (json) {
                 console.log('parsed json', json);
                 that.setState(foo => (json));
             })
-            .catch(function(ex) {
+            .catch(function (ex) {
                 console.log('parsing failed, URL bad, network down, or similar', ex);
             });
     };
@@ -30,18 +30,22 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-            <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>Welcome to React</h2>
-        </div>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
 
-        <p className="App-intro">
-            state: {this.state.status} file: {this.state.file}
-    </p>
-        <button onClick={this.queryServer}>Bar</button>
-        </div>
-    );
+                <p className="App-intro">
+            state: {this.state.status}
+                </p>
+                <p className="App-intro">
+            file: {this.state.file}
+                </p>
+                <button onClick={this.bar}>Bar</button>
+            </div>
+        );
     }
 }
+
 
 export default App;
--- Week02-RestBasics/client/src/App.test.js
+++ Week02-RestBasics/client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+describe('rest basic tests', function() {
+
+    it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders initial value of paragraph with state.nine', () => {
+    const wrapper = shallow(<App/>);
+    const unknown = <p className="App-intro">file: unknown</p>;
+    expect(wrapper.contains(unknown)).toEqual(true);
+});
+
 });
