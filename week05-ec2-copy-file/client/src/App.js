a64f11fcb12eed557b01ae7e54e9b301d19bf437
matttwilkes <matthew_wilkes7@hotmail.com>
2018-10-26 14:38:51 -0700

    ec2copyfile

---
+++ week05-ec2-copy-file/client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    copyScript = () => {
+        fetch('/script-pusher/copy-script')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Copy File</h1>
+                </header>
+                    <button onClick={this.copyScript}>Copy Script</button>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ week05-ec2-copy-file/server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/copy-script', function(request, response) { 'use strict';
+    response.send({ result: 'success' });
+});
+
+
+module.exports = router;
