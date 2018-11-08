2b9dd3ad59fb813d6096536bc25df2ff3aeec8c6
matttwilkes <matthew_wilkes7@hotmail.com>
2018-11-08 13:35:08 -0800

    Created script to build client and copy to server

--- week07-system-check-refactor/server/app.js
+++ week07-system-check-refactor/server/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const scriptPusher = require('./routes/script-pusher');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/script-pusher', scriptPusher);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
