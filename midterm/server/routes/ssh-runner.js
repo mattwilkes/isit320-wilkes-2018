d90eaba9891cf96dcf05903ac64d20ec36390eec
matttwilkes <matthew_wilkes7@hotmail.com>
2018-11-20 16:55:23 -0800

    system service control

--- midterm/server/routes/ssh-runner.js
+++ midterm/server/routes/ssh-runner.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const elfUtils = require('elven-code').elfUtils;
 
 const check = (request, response, next) => {
     console.log('REQUEST CHECK CALLED', request.query);
@@ -20,6 +21,31 @@ const check = (request, response, next) => {
 };
 
 router.use(check);
+
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                //var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+                var pattern = new RegExp('Host ec2-bc\n\t(.*)\n\t(.*)\n\t(.*)\n\t(.*)');
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
 
 const runCpuInfo = (hostAddress, response) => {
     var conn = new Client();
@@ -56,3 +82,15 @@ const runCpuInfo = (hostAddress, response) => {
         )
     });
 };
+
+router.get('/uptime', function(request, response) {
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            runUptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
+});
